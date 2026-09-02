aca3de63ed3d8e06f48ed0672279d01b0402e8d4
Prasetia24 <adamprasetia9@gmail.com>
2026-09-02 18:06:05 +0700

    Delete src directory

--- src/js/toast.js
+++
@@ -1,9 +0,0 @@
-// ===== TOAST =====
-function showToast(msg, isError = false) {
-  const toast = document.getElementById("toast");
-  toast.textContent = msg;
-  toast.style.background = isError ? "#e04a4a" : "var(--gold)";
-  toast.style.color = isError ? "#fff" : "#000";
-  toast.classList.add("show");
-  setTimeout(() => toast.classList.remove("show"), 3000);
-}
